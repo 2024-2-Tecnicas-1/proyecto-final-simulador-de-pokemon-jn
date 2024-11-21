67d4854fdd5797ee4b758c054517395dc4067c91
nicolas <jhoann.benavidesa@konradlorenz.edu.co>
2024-11-20 21:23:24 -0500

    se agrega el metodo batalla en la clase batalla

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,18 @@
 package simulador.batalla;
 
 public class Batalla {
-    // TODO: Aquí va tu código
+    public void iniciarBatalla(Pokemon pokemon1,Pokemon pokemon2){
+        System.out.println("Comienza la Batalla " + pokemon1.nombre + " vs " + pokemon2.nombre);
+        while(pokemon1.salud >0 && pokemon2.salud >0){
+            pokemon1.atacar(pokemon2);
+            if(pokemon2.salud >0){
+                pokemon2.atacar(pokemon1);
+            }
+        }
+        if(pokemon1.salud >0){
+            System.out.println(pokemon1.nombre + " ha ganado la batalla pokemon");
+        } else {
+            System.out.println(pokemon2.nombre + " ha ganado la batalla pokemon");
+        }
+    }
 }
