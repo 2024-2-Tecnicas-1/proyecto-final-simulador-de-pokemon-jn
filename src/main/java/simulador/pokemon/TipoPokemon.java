b17be4290732b51b99cb19c4fa7f10ff2a1b0935
nicolas <jhoann.benavidesa@konradlorenz.edu.co>
2024-11-20 22:25:55 -0500

    se arreglo los valores a multiplicador de daño

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -3,68 +3,37 @@ import java.util.List;
 
 public enum TipoPokemon {
     FUEGO,AGUA,PLANTA,ELECTRICO,VENENO,PSIQUICO,HADA,TIERRA,HIELO,ROCA,VOLADOR,FANTASMA;
-    public static double obtenerMultiplicadorDeDaño(TipoPokemon atacante, List<TipoPokemon> defensores){
-        double multiplicador=1.0;
-        for(TipoPokemon defensor:defensores){
-            multiplicador*=obtenerEfectividad(atacante, defensor); 
-        }
-        return multiplicador;
-    }
-     private static double obtenerEfectividad(TipoPokemon atacante,TipoPokemon defensor){
-       if(atacante==FUEGO){
-           if (defensor==PLANTA) return 2.0;
-           if (defensor==AGUA) return 0.5;
-           if (defensor==ELECTRICO) return 1.0;
-           if (defensor==TIERRA) return 0.5;
-           if (defensor==ROCA) return 0.5;
-       }
-       if(atacante==AGUA){
-           if (defensor==FUEGO) return 2.0;
-           if (defensor==PLANTA) return 0.5;
-           if (defensor==ELECTRICO) return 0.5;
-           if (defensor==TIERRA) return 2.0;
-       }
-       if(atacante==PLANTA){
-           if (defensor==AGUA) return 2.0;
-           if (defensor==FUEGO) return 0.5;
-           if (defensor==TIERRA) return 2.0;
-           if (defensor==VOLADOR) return 0.5;
-       }
-       if(atacante==ELECTRICO){
-           if (defensor==AGUA) return 2.0;
-           if (defensor==TIERRA) return 0.0;
-           if (defensor==VOLADOR) return 1.0;
-       }
-       if(atacante==VENENO){
-           if (defensor==PLANTA) return 2.0;
-           if (defensor==VENENO) return 0.5;
-           if (defensor==HADA) return 2.0;
-       }
-       if(atacante==PSIQUICO){
-           if (defensor==PSIQUICO) return 0.5;
-           if (defensor==FANTASMA) return 2.0;
-       }
-       if(atacante==HADA){
-           if (defensor==FANTASMA) return 1.0;
-       }
-       if(atacante==TIERRA){
-           if (defensor==AGUA) return 0.5;
-           if (defensor==FUEGO) return 2.0;
-       }
-       if(atacante==HIELO){
-           if (defensor==PLANTA) return 2.0;
-           if (defensor==AGUA) return 0.5;
-     }
-       if(atacante==ROCA){
-           if (defensor==FUEGO) return 2.0;
-           if (defensor==AGUA) return 0.5;
-}
-        if(atacante==VOLADOR){
-           if (defensor==TIERRA) return 2.0;      
-}
-        if(atacante==FANTASMA){
-           if (defensor==FANTASMA) return 2.0;
-}
+    public static double multiplicarDaño(TipoPokemon atacante, TipoPokemon defensor){
+        if (atacante == FUEGO && defensor == PLANTA) return 2.0;
+        if (atacante == FUEGO && defensor == AGUA) return 0.5;
+        if (atacante == FUEGO && defensor == ELECTRICO) return 1.0;
+        if (atacante == FUEGO && defensor == TIERRA) return 0.5;
+        if (atacante == FUEGO && defensor == ROCA) return 0.5;
+        if (atacante == AGUA && defensor == FUEGO) return 2.0;
+        if (atacante == AGUA && defensor == PLANTA) return 0.5;
+        if (atacante == AGUA && defensor == ELECTRICO) return 0.5;
+        if (atacante == AGUA && defensor == TIERRA) return 2.0;
+        if (atacante == PLANTA && defensor == AGUA) return 2.0;
+        if (atacante == PLANTA && defensor == FUEGO) return 0.5;
+        if (atacante == PLANTA && defensor == TIERRA) return 2.0;
+        if (atacante == PLANTA && defensor == VOLADOR) return 0.5;
+        if (atacante == ELECTRICO && defensor == AGUA) return 2.0;
+        if (atacante == ELECTRICO && defensor == TIERRA) return 0.0;
+        if (atacante == ELECTRICO && defensor == VOLADOR) return 1.0;
+        if (atacante == VENENO && defensor == PLANTA) return 2.0;
+        if (atacante == VENENO && defensor == VENENO) return 0.5;
+        if (atacante == VENENO && defensor == HADA) return 2.0;
+        if (atacante == PSIQUICO && defensor == PSIQUICO) return 0.5;
+        if (atacante == PSIQUICO && defensor == FANTASMA) return 2.0;
+        if (atacante == HADA && defensor == FANTASMA) return 1.0;
+        if (atacante == TIERRA && defensor == AGUA) return 0.5;
+        if (atacante == TIERRA && defensor == FUEGO) return 2.0;
+        if (atacante == HIELO && defensor == PLANTA) return 2.0;
+        if (atacante == HIELO && defensor == AGUA) return 0.5;
+        if (atacante == ROCA && defensor == FUEGO) return 1.0;
+        if (atacante == ROCA && defensor == AGUA) return 1.0;
+        if (atacante == VOLADOR && defensor == TIERRA) return 2.0;
+        if (atacante == FANTASMA && defensor == FANTASMA) return 2.0;
         return 1.0;
-     }
-     }
+    }
+}
