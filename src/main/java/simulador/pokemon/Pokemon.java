ea5a93e2bbc9b815cd94541b7f22137439ed7bb0
nicolas <jhoann.benavidesa@konradlorenz.edu.co>
2024-11-20 20:59:14 -0500

    agrego las propiedades y metodos en la clase pokemon

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,33 @@
 package simulador.pokemon;
+public abstract class Pokemon {
+protected String nombre;
+protected List<TipoPokemon> tipo;
+protected String estado;
+protected int salud;
+protected int ataque;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public Pokemon(String nombre,int salud,int ataque, List<TipoPokemon> tipo, String estado){
+    this.nombre=nombre;
+    this.tipo=tipo;
+    this.estado="normal";
+    this.salud=salud;
+    this.ataque=ataque;
+}
+public abstract void atacar(Pokemon oponente);
+public void recibirDaño(int daño){
+    this.salud-= daño;
+    if(this.salud<=0){
+        this.salud=0;
+        this.estado="Debilitado";
+    }
+}
+public void entrenar(){
+    this.ataque += 5;
+    this.salud += 10;
+    System.out.println(nombre +" ha mejorado las estadisticas");
+}
+@Override
+    public String toString() {
+        return nombre + " (Tipo: " + tipo + ", Salud: " + salud + ", Ataque: " + ataque + ", Estado: " + estado + ")";
+    }
 }
